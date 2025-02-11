df2b94d228481ec79e92311c97cac6d436d0ba8d
Nikhil-John-CL <johnnikhil511@gmail.com>
2025-02-11 13:14:55 +0530

    first commit my webpage

---
+++ script.js
@@ -0,0 +1,28 @@
+.container{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+.pro{
+
+    padding: 30px 25px;
+    box-shadow: 30px 20px 34px rgba(0, 0,0, 0.03);
+}
+.pro{
+        background-color:whitesmoke;
+        padding: 30px 25px;
+        border-width: 300px;
+        border: 1px solid black;
+        border-radius: 20px;  
+    
+    }
+
+.pro2{
+    display: block;
+    background-color:whitesmoke;
+    padding: 30px 25px;
+    border-width: 300px;
+    border: 1px solid black;
+    border-radius: 20px;  
+
+}
